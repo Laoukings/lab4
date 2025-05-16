79e2057fc15f2eef87d82b78de38a3b0b2a03a74
Laoukings <Lukpe828@student.liu.se>
2025-05-16 09:10:19 +0200

    lab4 union and find done

---
+++ lab4/code/code4b/dsets.cpp
@@ -0,0 +1,74 @@
+/*********************************************
+ * file:	~\code4b\dsets.cpp                *
+ * remark: implementation of disjoint sets    *
+ **********************************************/
+
+#include <iostream>
+#include <format>
+#include <cassert>
+
+#include "dsets.h"
+
+// -- CONSTRUCTORS
+
+DSets::DSets(int theSize) : V(theSize + 1) {  // slot zero is not used
+    assert(theSize > 0);
+    init();
+}
+
+// -- MEMBER FUNCTIONS
+
+// create initial sets
+void DSets::init() {
+    for (auto i = 1; i < std::ssize(V); ++i) {
+        V[i] = -1;  // every disjoint set has one node -- singleton
+    }
+}
+
+// join sets named r and s where r != s
+// i.e. join trees with roots r and s
+void DSets::join(int r, int s) {
+    assert(r != s);
+    assert(r >= 1 && r <= std::ssize(V) - 1);
+    assert(s >= 1 && s <= std::ssize(V) - 1);
+    assert(V[r] < 0);
+    assert(V[s] < 0);
+
+
+    if (V[s] < V[r]) {
+        V[s] += V[r];// update size of s's tree
+        V[r] = s;// r now points to s (s is new root)
+    }
+    else {
+        V[r] += V[s];// update size of r's tree
+        V[s] = r;// s now points to r (r is new root)
+    }
+}
+
+// return name of current set for x
+// i.e. return root of tree for x
+int DSets::find(int x) {
+    assert(x >= 1 && x <= std::ssize(V) - 1);
+    // path compression, also updates the parent of each element along the path to point directly to the root. 
+    if (V[x] < 0) {
+        return x;
+    } else {
+        return V[x] = find(V[x]); //Path compression: update V[x] to point directly to the root
+    }
+  
+}
+
+// just in case ...
+void DSets::print() const {
+    std::cout << "\n";
+
+    for (auto i = 1; i < std::ssize(V); i++) {
+        std::cout << std::format("{:4}", i);
+    }
+    std::cout << "\n";
+
+    for (auto u : V) {
+        std::cout << std::format("{:4}", u);
+    }
+    std::cout << "\n";
+}
